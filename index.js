e07da0903785b2d3b9c46f2963825d3200be6792
steven <bgomez.carrillo@fuac.edu.co>
2021-05-22 22:16:42 -0500

    Yo steven con ayuda de jhon he creado el login ajajjaja

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ firebase.auth().createUserWithEmailAndPassword(username, password)
         console.log(error);
     }
 });
+
+firebase.auth().signInWithEmailAndPassword(username, password)
+.then((credenciales) => {
+    console.log("login")
+    console.log(credenciales.user)
+})
+.catch((error) =>  {
+    if (error.code == "auth/email-already-in-use") {
+        alert("La dirección de correo electrónico ya está siendo utilizada por otra cuenta.");
+    } else {
+        console.log(error);
+    }
+});
